81f050e80c4e556b7a17a7f02ddab3f390307c8d
kikiiey <105841101321@student.unismuh.ac.id>
2024-05-27 14:49:26 +0800

    component

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
-import  ButtonCustom  from './button';
-import TextCustom from './text'
-import Images1 from './image';
-export default function App() {
-  return (
-    <>
-      <TextCustom/>
-      <Images1 />
-      <ButtonCustom/>
-    </>
-  );
-}
+const ButtonCustom = () => {
+    return (
+      <View style={{
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center', //kolom
+        alignItems: 'flex-end', //baris
+        flexDirection: 'row',
+        marginBottom: 100,
+      }}>
+        <Buttoncomponent backgroundColor='red'text='Login'/>
+        <Buttoncomponent backgroundColor='green'text='Register'/>
+      </View >
+    )
+  }
+  const Buttoncomponent = ({backgroundColor,text}) => {
+    return (
+      <View style={{
+        backgroundColor:backgroundColor,
+        width:150,
+        height:70,
+        borderRadius:10,
+        marginLeft:10
+      }}>
+      <Text style={{
+        color:'white',
+        textAlign:'center',
+        lineHeight:70,
+        fontSize:25,
+        fontWeight:'bold'
+      }}>
+        {text}
+      </Text>
+      </View>
+    )
+  }
+  export default ButtonCustom;
---
+++ component/button.js
@@ -0,0 +1,19 @@
+const Buttoncomponent = ({backgroundColor, text}) => {
+    return (
+      <View style={{
+        backgroundColor: backgroundColor, width: 150, height: 70,
+        borderRadius: 10,
+        marginLeft: 10,
+      }}>
+      <Text style={{
+        color: 'white',
+        textAlign: 'center',
+        lineHeight: 70,
+        fontSize: 25,
+        fontWeight: 'bold'
+      }}>
+        {text}
+      </Text>
+      </View>
+    )
+  }
